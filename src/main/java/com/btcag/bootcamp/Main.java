5b8ce08492ac22720f8966d16981d1ee07cc264a
STKUSHNI <stanislau.kushniarou@btc-ag.com>
2024-09-20 09:43:59 +0200

    Spielfeld generieren

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -3,5 +3,21 @@ package com.btcag.bootcamp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String [][] spielFeld = new String [15][10];
+        for (int i = 0; i < spielFeld.length; i++) {
+            for (int j = 0; j < spielFeld[i].length; j++) {
+                spielFeld[i][j] = "[ ]";
+
+            }
+        }
+
+        for (int i = 0; i < spielFeld.length; i++) {
+            for (int j = 0; j < spielFeld[i].length; j++) {
+                System.out.print(spielFeld[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
